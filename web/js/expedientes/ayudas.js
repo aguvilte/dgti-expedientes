7a7a25b35a8071f4c93bf9a498c4684e80d9bb5c
jaguerow <javieraguero154@gmail.com>
2019-05-08 09:03:26 -0300

    preparando sistema base

--- web/js/expedientes/ayudas.js
+++
@@ -1,6 +0,0 @@
-var inputFecha = document.getElementById('ayudas-fecha_entrada');
-    form = document.getElementById('w0');
-
-var date = inputFecha.value;
-var newdate = date.split('-').reverse().join('/');
-document.getElementById('ayudas-fecha_entrada').value = newdate;
