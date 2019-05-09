c9e495d0f9cd76f3647581f4daca4ff7de701427
jaguerow <javieraguero154@gmail.com>
2019-05-09 12:44:20 -0300

    vista site

--- views/site/index.php
+++ views/site/index.php
@@ -13,13 +13,13 @@ $this->title = 'SSE - Sistema de Seguimiento de Expedientes';
     </div>
 
     <div class="body-content">
-        <div>
+      <div>
 
               <?php if ($model->id_rol==1 or $model->id_rol==2) {
                 echo '
                 <div class="panel panel-primary">
                   <div class="panel-heading">
-                        <h2 class="titulo-seccion">ROL 1 (nombre del area al q pertenece el usuario?)</h2>
+                        <h2 class="titulo-seccion">Gestión Pública</h2>
                   </div>
                   <div class="panel-body">';
 
@@ -29,7 +29,7 @@ $this->title = 'SSE - Sistema de Seguimiento de Expedientes';
                     echo '
                     <article class="grid col-one-quarter mq3-col-full">
                      <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_beneficiarios.png') . Yii::t('app','REGISTRAR'), ['/beneficiarios/index']);
+                      echo Html::a(Html::img('@web/img/ico_beneficiarios.png') . Yii::t('app','AREAS'), ['/beneficiarios/index']);
                     echo '
                      </div>
                     </article>';
@@ -38,7 +38,7 @@ $this->title = 'SSE - Sistema de Seguimiento de Expedientes';
                     echo '
                     <article class="grid col-one-quarter mq3-col-full">
                      <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_consulta.png') . Yii::t('app','AYUDAS'), ['/ayudas/filters']);
+                      echo Html::a(Html::img('@web/img/ico_consulta.png') . Yii::t('app','CODIGOS'), ['/ayudas/filters']);
                     echo '
                      </div>
                     </article>';
@@ -47,33 +47,96 @@ $this->title = 'SSE - Sistema de Seguimiento de Expedientes';
                     echo '
                     <article class="grid col-one-quarter mq3-col-full">
                      <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','CONSULTA POR FECHA DE PAGO'), ['/fecha-pago/fecha_pago']);
+                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','RESOLUCIONES'), ['/fecha-pago/fecha_pago']);
                     echo '
                      </div>
                     </article>';
                   }
+                }
+
+              } 
+              echo '</div>';?>
+
+          </div>
+        <div>
+
+              <?php if ($model->id_rol==1 or $model->id_rol==3) {
+                echo '
+                <div class="panel panel-primary">
+                  <div class="panel-heading">
+                        <h2 class="titulo-seccion">ROL 3 (nombre del area al q pertenece el usuario?)</h2>
+                  </div>
+                  <div class="panel-body">';
+
+                foreach ($UsuariosSecciones as $UsuariosSeccion) {
+
                   if ($UsuariosSeccion->id_seccion==4) {
                     echo '
                     <article class="grid col-one-quarter mq3-col-full">
                      <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','CONSULTA POR FECHA DE ENTRADA'), ['/fecha-entrada/fecha_entrada']);
+                      echo Html::a(Html::img('@web/img/ico_beneficiarios.png') . Yii::t('app','CONSULTA CODIGO'), ['/beneficiarios/index']);
+                    echo '
+                     </div>
+                    </article>';
+                  }
+                  if ($UsuariosSeccion->id_seccion==5) {
+                    echo '
+                    <article class="grid col-one-quarter mq3-col-full">
+                     <div class="ipanel_icon ">';
+                      echo Html::a(Html::img('@web/img/ico_consulta.png') . Yii::t('app','CARGA Y ENVIO'), ['/ayudas/filters']);
+                    echo '
+                     </div>
+                    </article>';
+                  }
+                  if ($UsuariosSeccion->id_seccion==6) {
+                    echo '
+                    <article class="grid col-one-quarter mq3-col-full">
+                     <div class="ipanel_icon ">';
+                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','RECIBIR'), ['/fecha-pago/fecha_pago']);
+                    echo '
+                     </div>
+                    </article>';
+                  }
+                  if ($UsuariosSeccion->id_seccion==7) {
+                    echo '
+                    <article class="grid col-one-quarter mq3-col-full">
+                     <div class="ipanel_icon ">';
+                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','RECORRIDO'), ['/fecha-pago/fecha_pago']);
                     echo '
                      </div>
                     </article>';
                   }
                 }
               echo '</div>';
-              ?>
+              
+              } ?>        
 
-              <?php
-                echo '<div class="panel-body">';
+          </div>
+          <div>
+              <?php if ($model->id_rol==1 or $model->id_rol==4) {
+                echo '
+                <div class="panel panel-primary">
+                  <div class="panel-heading">
+                        <h2 class="titulo-seccion">CONTADURÍA</h2>
+                  </div>
+                  <div class="panel-body">';
 
                 foreach ($UsuariosSecciones as $UsuariosSeccion) {
-                  if ($UsuariosSeccion->id_seccion==5) {
+
+                  if ($UsuariosSeccion->id_seccion==8) {
                     echo '
                     <article class="grid col-one-quarter mq3-col-full">
                      <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_movimientos.png') . Yii::t('app','MOVIMIENTOS'), ['/movimientos/index']);
+                      echo Html::a(Html::img('@web/img/ico_beneficiarios.png') . Yii::t('app','CONSULTA CODIGO'), ['/beneficiarios/index']);
+                    echo '
+                     </div>
+                    </article>';
+                  }
+                  if ($UsuariosSeccion->id_seccion==9) {
+                    echo '
+                    <article class="grid col-one-quarter mq3-col-full">
+                     <div class="ipanel_icon ">';
+                      echo Html::a(Html::img('@web/img/ico_consulta.png') . Yii::t('app','CARGA Y ENVIO'), ['/ayudas/filters']);
                     echo '
                      </div>
                     </article>';
@@ -82,80 +145,16 @@ $this->title = 'SSE - Sistema de Seguimiento de Expedientes';
                     echo '
                     <article class="grid col-one-quarter mq3-col-full">
                      <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_informes.png') . Yii::t('app','LISTADOS'), ['/listado/index']);
+                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','RECIBIR'), ['/fecha-pago/fecha_pago']);
                     echo '
                      </div>
                     </article>';
                   }
-                }
-              echo '</div>';
-              ?>
-
-            <?php } ?>
-
-          </div>
-
-
-            <?php if ($model->id_rol==1 or $model->id_rol==3) {
-                echo '
-                <div class="panel panel-primary">
-                  <div class="panel-heading">
-                        <h2 class="titulo-seccion">Contaduría</h2>
-                  </div>
-                  <div class="panel-body">';
-
-                foreach ($UsuariosSecciones as $UsuariosSeccion) {
-
-                  if ($UsuariosSeccion->id_seccion==6) {
-                      echo '
-                      <article class="grid col-one-quarter mq3-col-full">
-                       <div class="ipanel_icon ">';
-                        echo Html::a(Html::img('@web/img/ico_orden_pago.png') . Yii::t('app','PAGO'), ['/pago/filters']);
-                      echo '
-                       </div>
-                      </article>';
-                    }
-                  if ($UsuariosSeccion->id_seccion==7) {
-                    echo '
-                    <article class="grid col-one-quarter mq3-col-full">
-                     <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','CONSULTA POR FECHA DE PAGO'), ['/fecha-pago/fecha_pago']);
-                    echo '
-                     </div>
-                    </article>';
-                  }
-                  if ($UsuariosSeccion->id_seccion==8) {
-                    echo '
-                    <article class="grid col-one-quarter mq3-col-full">
-                     <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','CONSULTA POR FECHA DE ENTRADA'), ['/fecha-entrada/fecha_entrada']);
-                    echo '
-                     </div>
-                    </article>';
-                  }
-                   if ($UsuariosSeccion->id_seccion==9) {
-                    echo '
-                    <article class="grid col-one-quarter mq3-col-full">
-                     <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_movimientos.png') . Yii::t('app','MOVIMIENTOS'), ['/movimientos/index']);
-                    echo '
-                     </div>
-                    </article>';
-                  }
-                }
-              echo '</div>';
-              ?>
-
-              <?php
-                echo '<div class="panel-body">';
-
-                foreach ($UsuariosSecciones as $UsuariosSeccion) {
-
                   if ($UsuariosSeccion->id_seccion==11) {
                     echo '
                     <article class="grid col-one-quarter mq3-col-full">
                      <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_informes.png') . Yii::t('app','LISTADOS'), ['/listado/index']);
+                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','RECORRIDO'), ['/fecha-pago/fecha_pago']);
                     echo '
                      </div>
                     </article>';
@@ -164,35 +163,16 @@ $this->title = 'SSE - Sistema de Seguimiento de Expedientes';
                     echo '
                     <article class="grid col-one-quarter mq3-col-full">
                      <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_expedientes.png') . Yii::t('app','EXPEDIENTES'), ['/expedientes/index']);
-                    echo '
-                     </div>
-                    </article>';
-                  }
-                  if ($UsuariosSeccion->id_seccion==13) {
-                    echo '
-                    <article class="grid col-one-quarter mq3-col-full">
-                     <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_estructuras.png') . Yii::t('app','ÁREAS'), ['/areas/index']);
-                    echo '
-                     </div>
-                    </article>';
-                  }
-                  if ($UsuariosSeccion->id_seccion==14) {
-                    echo '
-                    <article class="grid col-one-quarter mq3-col-full">
-                     <div class="ipanel_icon ">';
-                      echo Html::a(Html::img('@web/img/ico_no_escalafonados.png') . Yii::t('app','REFERENTES'), ['/referentes/index']);
+                      echo Html::a(Html::img('@web/img/ico_historial.png') . Yii::t('app','ORDENES DE PAGO'), ['/fecha-pago/fecha_pago']);
                     echo '
                      </div>
                     </article>';
                   }
                 }
               echo '</div>';
-              ?>
-
-            <?php } ?>
-
+              
+              } ?> 
+            </div>
                             <!--
                             <article class="grid col-one-quarter mq3-col-full">
                                 <div class="ipanel_icon"><?php //Html::a(Html::img('@web/img/ico_modificaciones.png') . Yii::t('app','MODIFICACIÓN'), ['/registros/modificacion']) ?></div>
@@ -200,4 +180,3 @@ $this->title = 'SSE - Sistema de Seguimiento de Expedientes';
 
         </div>
     </div>
-</div>
